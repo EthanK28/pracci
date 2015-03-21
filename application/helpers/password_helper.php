e35db9bd6f9568dfd505929b22664622137ab110
ethank <kes0228@gmail.com>
2015-03-21 10:20:51 +0900

    맥북 실

---
+++ application/helpers/password_helper.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: es-mac
+ * Date: 15. 3. 19.
+ * Time: 오후 12:10
+ */
---
+++ application/models/user_model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: es-mac
+ * Date: 15. 3. 19.
+ * Time: 오전 11:42
+ */
